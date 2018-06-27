0f8077ca30949326e2cdfd2f2716ffa24108541e
Subarno <subarnopal.babai@gmail.com>
2018-06-27 10:27:51 +0530

    lstm update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # TimeSeriesPredictionLSTM
+look_back = 1
+Train Score: 0.57 RMSE
+Test Score: 1.99 RMSE
+
+look_back = 10
+Train Score: 0.54 RMSE
+Test Score: 17.01 RMSE
+
+look_back = 5
+Train Score: 0.58 RMSE
+Test Score: 1.58 RMSE
+
+look_back = 3
+Train Score: 0.55 RMSE
+Test Score: 1.70 RMSE
+
--- lstm.py
+++ lstm.py
@@ -1,8 +1,86 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.utils import plot_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
-dataset = pd.read_excel('real-daily-wages-in-pounds-england.xlsx',dtype={'Wage':float})
-#print(dataset.Wage)
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return np.array(dataX), np.array(dataY)
 
-plt.plot(dataset.Wage)
+np.random.seed(7)
+
+dataframe = pd.read_excel('real-daily-wages-in-pounds-england.xlsx',dtype={'Wage':float})
+plt.plot(dataframe.Wage, color='b')
+plt.title('Dataset Plot')
 plt.savefig('img/data_plot.png')
+dataset = pd.DataFrame(dataframe.Wage)
+dataset = dataset.astype('float32')
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+
+# reshape into X=t and Y=t+1
+look_back = 4
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+# reshape input to be [samples, time steps, features]
+trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+
+print(trainX.shape, trainY.shape)
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=( look_back, 1)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+plot_model(model, to_file='img/model_lstm.png')
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# shift train predictions for plotting
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.title('LSTM Result look_back=4 epoch 100')
+plt.plot(scaler.inverse_transform(dataset), color='b' )
+plt.plot(trainPredictPlot, color='g', label='Train Rsult' )
+plt.plot(testPredictPlot, color='r', label='Test Result')
+plt.legend(loc='upper left')
+plt.savefig('img/result_plot_lstm_4.png')
